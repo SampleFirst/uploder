cec974f02def3e637247fabe5ea27d666f71d420
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-13 20:42:16 +0530

    Create admin.py

---
+++ plugins/admin.py
@@ -0,0 +1,23 @@
+from pyrogram import Client as Clinton
+from pyrogram import filters, enums
+from config import Config
+from database.access import clinton
+from plugins.buttons import *
+
+@Clinton.on_message(filters.private & filters.command('total'))
+async def sts(c, m):
+    if m.from_user.id != Config.OWNER_ID:
+        return 
+    total_users = await clinton.total_users_count()
+    await m.reply_text(text=f"Total user(s) {total_users}", quote=True)
+
+
+@Clinton.on_message(filters.private & filters.command("search"))
+async def serc(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id, 
+        text="🔍 TORRENT SEARCH", 
+        parse_mode=enums.ParseMode.HTML, 
+        reply_markup=Button.BUTTONS01
+    )
+    
